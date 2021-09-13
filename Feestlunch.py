4d7ac05a9bcdb3e4996781053f34b335193b872e
rouamu <Roua.mulham@gmail.com>
2021-09-13 14:23:40 +0200

    first

--- Feestlunch.py
+++ Feestlunch.py
@@ -1,11 +1,14 @@
-croissantjes = (17)
-prijs = (0.39)
-stokbroden = (2)
-Prijs = (2.78)
-kortingsbonnen = (1.50)
-totaal = (croissantjes * prijs + stokbroden * Prijs - kortingsbonnen)
+croissantjes = float(input('antaal'))
+prijscroi = (39)
+stokbroden =float(input('antaal'))
+Prijsstok = (278)
+kortingsbonnen =float(input('korting'))
+kortingkosten =50
 
-print("De feestlunch kost je bij de bakker 18.88 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!" + str(totaal))
+totaal = (croissantjes)*(prijscroi)+(stokbroden)*(Prijsstok)-(kortingsbonnen)*(kortingkosten)
+
+print('je factuer is: '+ str(totaal))
+
 
 
 
