54f4aaa30c87ad250862ad00c4f264346bac7659
rouamu <Roua.mulham@gmail.com>
2021-09-13 11:18:59 +0200

    person.py

---
+++ person.py
@@ -0,0 +1,29 @@
+Naam =  input("wat is je naam? ")
+print(Naam)
+
+Adres = input("wat is jou adres? ")
+print(Adres)
+
+postcode = input("wat is jou postcode? ")
+print(postcode)
+
+woonplaats = input("waar woon je?")
+print(woonplaats)
+
+
+print('----------------------------------------------------')
+print('Naam =      ' + Naam)              
+print('Adres =     ' +Adres)          
+print('postcode =  ' +postcode)                      
+print('Woonplaats = ' + woonplaats)                        
+print('----------------------------------------------------')
+
+
+
+
+
+
+
+
+
+
