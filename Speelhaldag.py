55d95cb39ef2be1f9658a6e8dd0a9074987249de
rouamu <Roua.mulham@gmail.com>
2021-09-13 13:30:10 +0200

    Speelhaldag.py

---
+++ Speelhaldag.py
@@ -0,0 +1,9 @@
+toegangsticket = 7.45 * 4
+games = 36 * 0.37
+
+totaal = (7.45 * 4 + 36 * 0.37)
+
+print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 44.44 euro" +str(totaal))
+
+
+
