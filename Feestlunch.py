360b8ebe177603949c957b3d4b0458a10fa777d5
rouamu <Roua.mulham@gmail.com>
2021-09-13 12:40:49 +0200

    Feestlunch.py

---
+++ Feestlunch.py
@@ -0,0 +1,12 @@
+croissantjes = (17)
+prijs = (0.39)
+stokbroden = (2)
+Prijs = (2.78)
+kortingsbonnen = (1.50)
+totaal = (croissantjes * prijs + stokbroden * Prijs - kortingsbonnen)
+
+print("De feestlunch kost je bij de bakker 18.88 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!" + str(totaal))
+
+
+
+
