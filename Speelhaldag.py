3b5ab2e7bd90c2df1ffaaaead5763bbd7d179781
Rouamu <89839446+Rouamu@users.noreply.github.com>
2021-09-13 13:41:31 +0200

    Delete Speelhaldag.py

--- Speelhaldag.py
+++
@@ -1,9 +0,0 @@
-toegangsticket = 7.45 * 4
-games = 36 * 0.37
-
-totaal = (7.45 * 4 + 36 * 0.37)
-
-print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 44.44 euro" +str(totaal))
-
-
-
